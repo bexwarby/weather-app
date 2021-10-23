7e4fa995062b93d58ea5fe625cad578c16dd6960
Rebekah Munoz <bex.warburton@gmail.com>
2021-10-23 10:09:25 +0200

    Better - need to display logo, description, style

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-/* L'icone correspondant aux conditions météo actuelles
-Le descriptif
-La température actuelle
-La vistesse du vent */
+/* App.js 
+* Main functions of the weather app
+*/
 
+/* IMPORTS */
 import React, { useState, useEffect } from 'react';
 import {
   Text,
@@ -12,72 +12,63 @@ import {
   StatusBar,
   TouchableOpacity
 } from 'react-native';
+/** Style imports */
 import styles from "./style/general.js";
 import top from "./style/top.js";
 import description from "./style/description.js";
 import wind from "./style/wind.js";
 import temp from "./style/temp.js";
 import current from "./style/current.js";
+/** Icon imports */
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome5 } from '@expo/vector-icons';
 import * as Location from "expo-location";
 
-const clouds = require("./cloud.jpg");
-/* const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const clouds = require("./cloud.jpg");/* 
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 const WEATHER_API_KEY = '0b626d564aefeb00bdbd2c294ee1df7'; */
 
 function App() {
 
+  const [errorMsg, setErrorMsg] = useState('');
+  const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState({});
+  const [logo, setLogo] = useState('');
   /*   const [unitsSystem, setUnitsSystem] = useState('metric')*/
 
-  async function getWeatherDataByLocation() {
-    /* Appel de la fonction getPosition pour récupérer la position */
-    const location = await getPosition();
-    /* Vérification de l'objet location */
-    if (!location.coords) {
-      Alert.alert(
-        "Impossible to find your location",
-        "Please activate on your phone."
-      );
+  React.useEffect(() => {
+    getWeatherDataByLocation();
+  }, []);
+
+  /** Call the devices coordinates and set in API */
+  const getWeatherDataByLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
       return;
     }
+    let location = await Location.getCurrentPositionAsync({});
+    /* set location state */
+    setLocation(location);
     const coords = location.coords;
-    
+    /* fetch api data */
     const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=0b626d564aefeb00bdbd2c294ee1df75&units=metric`
     );
     /* reads object JSON */
     const data = await response.json();
-    /* store data in weather const */
-    setWeather(data);/* 
-    console.log(data); */
-  }
-
-  async function getPosition() {
-    let permission = await Location.getForegroundPermissionsAsync();
-    if (permission.status == 'undetermined') {
-      permission = await Location.requestForegroundPermissionsAsync();
-    }
-    if (permission.status == 'granted') {
-      let location = await Location.getCurrentPositionAsync();
-      return location;
-    } else {
-      return {};
-    }
-  }
+    /* store data in weather state */
+    setWeather(data);
+    getWeatherLogo();
+  };
 
   // Getting weather logo
-  /* async function getWeatherLogo() {
+  async function getWeatherLogo() {
     const logo = Array.isArray(weather.weather) && weather.weather[0]
       ? weather.weather[0].icon : "-";
     setLogo(logo);
-    /* Where should I add:
-     * onChange={getWeatherLogo} 
-
-  };  
-};*/
-
+  }
+    
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
@@ -100,39 +91,43 @@ function App() {
             </View>
             <View style={top.currentLocation}>
               <Text style={top.textLocation}>
-                {weather.weather?.name ?? "-"}
+                {weather.name ?? "-"}
               </Text>
             </View> 
           </View>
         </View>
 
         <View style={styles.weather}>
-          {/* Top icons */}
+
+          {/* Left icons */}
           <View style={styles.topWeather}>
             {/* Current weather */}
             <View style={current.current}>
               <Text style={current.currentTitle}>Forecast</Text>
-              {/* Weather icon */}
+              
               <View style={current.weatherLogo}>
-                {/* <Image
+                {/* Weather icon */}
+                <Image
                   style={styles.stretch}
                   source={{
                     uri:
-                      'http://openweathermap.org/img/wn/' +
-                      icon +
-                      '@2x.png',
-                  }} />  */}
+                      "http://openweathermap.org/img/wn/" +
+                      {logo} +
+                      "@2x.png",
+                  }} />  
               </View>
             </View>
             {/* Description of weather */}
             <View style={description.description}>
               <Text style={description.descTitle}>Description</Text>
+              {/* Description text */}
               <Text style={description.textWeather}>
                 {weather.weather?.description ?? "-"}
               </Text>
             </View>
           </View>
-          {/* Bottom icons */}
+          
+          {/* Right icons */}
           <View style={styles.bottomWeather}>
             {/* Temperature */}
             <View style={temp.temperature}>
@@ -144,6 +139,7 @@ function App() {
                     size={50}
                     color='blue'
                   />
+                  {/* Minimum temperature */}
                   <Text style={temp.tempValue}>
                     {weather.main?.temp_min ?? "-"}
                   </Text>
@@ -154,6 +150,7 @@ function App() {
                     size={50}
                     color='red'
                   />
+                  {/* Maximum temperature */}
                   <Text style={temp.tempValue}>
                     {weather.main?.temp_max ?? "-"}
                   </Text>
@@ -166,6 +163,7 @@ function App() {
 
               <View style={wind.windIcon}>
                 <FontAwesome5 name="wind" size={35} color="blue" />
+                {/* Wind speed */}
                 <Text>
                   {weather.wind?.speed ?? "-"} km/h
                 </Text>
